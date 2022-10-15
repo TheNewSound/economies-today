fdc8e6cd8c61515c8249bb8506e0993a922993f7
v <info@worldwidecrap.nl>
2022-10-15 19:21:17 +0200

    Rename

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,6 @@
 <template>
   <svg xmlns="http://www.w3.org/2000/svg" style="background-color: rgb(255, 255, 255); width:100%;max-width:500px;"
-       xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1" viewBox="0 0 500 500"
-       content="&lt;mxfile host=&quot;app.diagrams.net&quot; modified=&quot;2022-09-22T19:59:24.294Z&quot; agent=&quot;5.0 (X11; Ubuntu)&quot; etag=&quot;W8Jy8Ku8wPk1btfwUcLf&quot; version=&quot;20.3.6&quot; type=&quot;device&quot;&gt;&lt;diagram id=&quot;nh4gvzxMUjmvN9fP-wya&quot; name=&quot;Page-1&quot;&gt;7Vpbb9owFP41eeyU2Elgj4VepVarxkPH0+QlJlh1bOY4XPrr5zTO1VBApaFiSAjFX3z9zudzfAwWHMbLW4Fm00ceYmoBO1xa8MoCwHEBsLKPHa5ypOf5ORAJEupKFTAir1iDtkZTEuKkUVFyTiWZNcGAM4YD2cCQEHzRrDbhtDnqDEXYAEYBoib6TEI5zdG+Z1f4HSbRtBjZsfWbGBWVNZBMUcgXNQheW3AoOJf5U7wcYpqRV/CSt7vZ8LacmMBM7tLggsX3wl2Oh4+v44fft/zHnUcvdC9zRFO94CfBwzSQScYfC9X3CIs5CXAGPCLxgqVejlwVHAmeshBnw9gWHCymROLRDAXZ24VShcKmMqaq5KhHPSAWEi83rsQp+VHCwjzGUqxUFd0A+l7eRGvKKSheVBYqsWnNOr7GkBZFVHZd8aYeNHV70AgMGh/QHy6+LmVu/9iUuQZld2mMWPIOVU4nVJU0aKrA0anyDKoGaUIYThJ8fLp8+6vR5UCDL4MlzMLLLDqoUkBRkpCgSUyQinlJIl4S+Svbqt88XRrrjZs9Xy3rhVVRYGopv+qFcdVDVqyavZWKdvlMcWhEpZY91Gp4KgK8fYdJJCIst8UA0741+3lrzFdgAlMkybw53XU21SM8caIWUsrHbcnHBS1Z5MvUrerhrd2R1+wI9lod5TwYHSkZoFWt2iyrkGyecHkyKYKP3wi76iHvsdJvyekHJG16ywNJui7oUt7bJF2peFz0142kwY6Sds+S3kPSTr814X4Xkjaj2mEk7RxA0nZ3ki6Smm2S9s6S3kPSvncML+13cvBwdpO0Yx3r4OHtKGlwlvQHJO3ZXUi6Z0j6hir1AptP1Bclf1MSvi0iwdJMRlRaIZv6TqTgL3jIqcqO4RXjTNUcTAilLQhRErFskyj1YYUPsiSFBIhe6hcxCcNsmLUpTjO9tg6QPxvkW0aSs06s8NOSnO+H9jUbEhawzXGsj7vg/cB7QG+za5pzDqD7eBu3leaAXgfepiDl8KJuBcPerqJu5ke9zkS9awg9Jzp7ibp1tQe7OBUC8479ns1xIgmLTjhg9lr+w4NmxHS6jJiFL6nZ4SeWqWBvdrBngk+IeVF/sgaBrntsg5iXWkPOkjQ+6Y1RZKLtzLRuB79TM5gXMflvgCdtBh9+NTOYlwfX8Yzy1X9lBndNYtWtGcyE95mLl5M2QtslfaIRVLH6x0N+wqr+NwKv/wE=&lt;/diagram&gt;&lt;/mxfile&gt;">
+       xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 500 500">
     <defs>
 
       <linearGradient id="liquid-gradient" bx:pinned="true" x1="0" x2="0" y1="0" y2="1">
@@ -11,11 +10,11 @@
       </linearGradient>
       <marker id="marker-arrowhead" viewBox="0 0 10 10" overflow="visible" orient="auto" refX="2" refY="5" markerWidth="2" markerHeight="2" xlink:href="#marker-arrowhead">
         <title>arrowhead</title>
-        <path class="liquid-arrowhead" d="M 10.92 8.378 Q 11.217 7.863 11.514 8.378 L 15.867 15.93 Q 16.164 16.445 15.57 16.445 L 6.864 16.445 Q 6.27 16.445 6.567 15.93 Z" transform="matrix(-0.000078, 1, -1, -0.000078, 17.746244, -6.216717)" bx:shape="triangle 6.27 7.863 9.894 8.582 0.5 0.06 1@5e4be6d4"></path>
+        <path class="liquid-arrowhead" d="M 10.92 8.378 Q 11.217 7.863 11.514 8.378 L 15.867 15.93 Q 16.164 16.445 15.57 16.445 L 6.864 16.445 Q 6.27 16.445 6.567 15.93 Z" transform="matrix(-0.000078, 1, -1, -0.000078, 17.746244, -6.216717)"></path>
       </marker>
       <marker id="marker-arrowhead-large" viewBox="0 0 10 10" overflow="visible" orient="auto" refX="2" refY="5" markerWidth="4" markerHeight="4" xlink:href="#marker-arrowhead">
         <title>arrowhead</title>
-        <path class="tax-arrowhead" d="M 10.92 8.378 Q 11.217 7.863 11.514 8.378 L 15.867 15.93 Q 16.164 16.445 15.57 16.445 L 6.864 16.445 Q 6.27 16.445 6.567 15.93 Z" transform="matrix(-0.000078, 1, -1, -0.000078, 17.746244, -6.216717)" bx:shape="triangle 6.27 7.863 9.894 8.582 0.5 0.06 1@5e4be6d4"></path>
+        <path class="tax-arrowhead" d="M 10.92 8.378 Q 11.217 7.863 11.514 8.378 L 15.867 15.93 Q 16.164 16.445 15.57 16.445 L 6.864 16.445 Q 6.27 16.445 6.567 15.93 Z" transform="matrix(-0.000078, 1, -1, -0.000078, 17.746244, -6.216717)"></path>
       </marker>
       <path id="mypath" d="M 0,50 C 0 15 15 0 50 0" class="flow-liquid" pointer-events="stroke"/>
     </defs>
@@ -82,17 +81,17 @@
       <path d="M 360,260 C 300 280 190 280 150 260" class="flow-liquid"/>
       <path d="M 140,260 C 190 280 300 280 350 260" id="returns" fill="none" stroke="none" />
       <text >
-        <textPath href="#investing" startOffset="50%" text-anchor="middle">Investing</textPath>
-        <textPath href="#returns" startOffset="50%" text-anchor="middle">Returning profit</textPath>
+        <textPath href="#investing" class="flow-label" startOffset="50%">Investing</textPath>
+        <textPath href="#returns" class="flow-label" startOffset="50%">Returning profit</textPath>
       </text>
       <text>
-        <textPath href="#consuming" startOffset="50%" text-anchor="middle">Consuming</textPath>
-        <textPath href="#producing" startOffset="50%" text-anchor="middle">Producing</textPath>
-        <textPath href="#utilizing" startOffset="50%" text-anchor="middle">Utilizing</textPath>
+        <textPath href="#consuming" class="flow-label" startOffset="50%">Consuming</textPath>
+        <textPath href="#producing" class="flow-label" startOffset="50%">Producing</textPath>
+        <textPath href="#utilizing" class="flow-label" startOffset="50%">Utilizing</textPath>
       </text>
       <text>
-        <textPath href="#employing" startOffset="50%" text-anchor="middle">Employing</textPath>
-        <textPath href="#working" startOffset="50%" text-anchor="middle">Working</textPath>
+        <textPath href="#employing" class="flow-label" startOffset="50%">Employing</textPath>
+        <textPath href="#working" class="flow-label" startOffset="50%">Working</textPath>
       </text>
 
       <path display="none" class="flow-tax" d="M 246.959 19.209 C 422.606 19.209 532.386 209.353 444.562 361.467 C 356.739 513.582 137.18 513.582 49.357 361.467 C -27.451 228.433 46.487 59.044 196.262 24.913" transform="matrix(0.104528, -0.994522, 0.994522, 0.104528, -24.797894, 467.221399)"></path>
@@ -167,7 +166,7 @@ export default {
   fill: none;
   stroke: rgb(137, 225, 245);
   paint-order: stroke;
-  stroke-width: 10px;
+  stroke-width: 12px;
 }
 .flow-tax {
   stroke-dashoffset: 0px;
@@ -177,6 +176,11 @@ export default {
   stroke: var(--color-tax);
   paint-order: stroke;
   stroke-width: 5px;
+}
+.flow-label {
+  font-size: 12px;
+  text-anchor: middle;
+  dominant-baseline: middle;
 }
 
 </style>
